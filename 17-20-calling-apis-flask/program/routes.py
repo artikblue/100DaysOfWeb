817d312e0ef08512bc6e726fb6ec9d151d2e92a1
artikblue <artikblue@protonmail.com>
2020-01-15 13:12:56 +0100

    moar work on the pokemon/pokedex micro app

--- 17-20-calling-apis-flask/program/routes.py
+++ 17-20-calling-apis-flask/program/routes.py
@@ -30,15 +30,49 @@ def pokemon():
         
         colour = request.form.get('pokecolour')
         pokemon = get_poke_colours(colour)
+
     return render_template('pokemon.html', pokemon = pokemon)
+
+def get_all_colours():
+    colours = []
+    r = requests.get('https://pokeapi.co/api/v2/pokemon-color/')
+    coloursdata = r.json()
+
+    for c in coloursdata["results"]:
+        colours.append(c["name"])
+    return colours
 
 def get_poke_colours(colour):
     pokemon = []
     r = requests.get('https://pokeapi.co/api/v2/pokemon-color/' + colour.lower())
-    pokedata = r.json()
-    
-    print(pokedata)
+    try:
+        pokedata = r.json()
+    except Exception as e:
+        print(e)
+        pokedata = []
 
-    for i in pokedata['pokemon_species']:
-        pokemon.append(i["name"])
+    if pokedata:
+        for i in pokedata['pokemon_species']:
+            pokemon_name = i["name"]
+            pokeinfo = get_poke_info(pokemon_name)
+
+            if pokeinfo:
+                pokeobject = {
+                    "name":pokemon_name,
+                    "order": pokeinfo["order"],
+                    "weight": pokeinfo["weight"],
+                    "height": pokeinfo["height"],
+                    "types": pokeinfo["types"],
+                    "area": pokeinfo["location_area_encounters"]
+                }
+                pokemon.append(pokeobject)
     return pokemon
+
+def get_poke_info(pokename):
+    pokeinfo = {}
+    r = requests.get('https://pokeapi.co/api/v2/pokemon/'+pokename)
+    try:
+        pokeinfo = r.json()
+    except Exception as e:
+        print(e)
+    return pokeinfo
